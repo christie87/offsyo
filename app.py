4c124293b7b8ac1bc65e3e2e4957bb625bd97fd5
christie87 <123386244+christie87@users.noreply.github.com>
2023-05-06 22:36:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ cuisine_list=df['Cuisines'].unique().tolist()
 
 
 
-app = Dash(__name__,external_stylesheets=[dbc.themes.SLATE])
+app = Dash(__name__,external_stylesheets=[dbc.themes.SLATE],suppress_callback_exceptions = True)
 
 server = app.server 
 
